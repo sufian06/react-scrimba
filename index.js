bef5496bcd7cc8d897fe81c2b6960140ecb58dc0
sufian <sufian4se@gmail.com>
2022-02-11 17:33:59 +0600

    first commit - file setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const hello = (
+    <nav>
+        <h1>Hello, World!</h1>
+    </nav>
+)
+
+ReactDOM.render(hello, document.getElementById("root"));
